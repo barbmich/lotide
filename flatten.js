1fe4db964ea52efb649a9b4ee18ef530797a9989
Michele Barbiero <babishhh@gmail.com>
2020-07-22 00:11:10 +0000

    commit of flatten.js

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return console.log(`✅ Assertion Passed! The two arrays are the same.`);
+  } else {
+    return console.log(`🛑 Assertion Failed! The two arrays are different.`);
+  }
+};
+
+const flatten = array => {
+  let output = [];
+  for (let index of array) {
+    if (Array.isArray(index)) {
+      for (let value of index) {
+        output.push(value);
+      }
+    } else {
+      output.push(index);
+    }
+  }
+  return output;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
