fd6a66ba7bc985d263fe3645cd853fc2774f26f2
Michele Barbiero <babishhh@gmail.com>
2020-07-22 18:50:54 +0000

    commit countLetters.js. Returns object in all lowercase.

---
+++ countLetters.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    if (typeof actual === 'string') {
+      return console.log(`✅ Assertion Passed! "${actual}" === "${expected}"`);
+    } else {
+      return console.log(`✅ Assertion Passed! ${actual} === ${expected}`);
+    }
+  } else {
+    if (typeof actual === 'string') {
+      return console.log(`🛑 Assertion Failed! "${actual}" !== "${expected}"`);
+    } else {
+      return console.log(`🛑 Assertion Failed! ${actual} !== ${expected}`);
+    }
+  }
+};
+
+const noSpaces = string => {
+  let output = string.split(' ').join('');
+  return output;
+}
+
+const lowerCase = string => {
+  let output = string.toLowerCase();
+  return output;
+}
+
+const countLetters = string => {
+  let output = {};
+  for (let char of string) {
+    if (output[char]) {
+      output[char]++;
+    } else {
+      output[char] = 1;
+    }
+  }
+  return output;
+};
+
+console.log(countLetters(lowerCase(noSpaces(`lIgHtHouse in the house`))))
