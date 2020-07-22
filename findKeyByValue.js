c889f58e2f65ce99c0b4ad485fc05f184b15a33e
Michele Barbiero <babishhh@gmail.com>
2020-07-22 20:07:15 +0000

    commit findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    if (typeof actual === 'string') {
+      return console.log(`✅ Assertion Passed! "${actual}" === "${expected}"`);
+    } else {
+      return console.log(`✅ Assertion Passed! ${actual} === ${expected}`);
+    }
+  } else {
+    if (typeof actual === 'string') {
+      return console.log(`🛑 Assertion Failed! "${actual}" !== "${expected}"`);
+    } else {
+      return console.log(`🛑 Assertion Failed! ${actual} !== ${expected}`);
+    }
+  }
+};
+
+const findKeyByValue = (object, value) => {
+  for (key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire")) // drama
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse")) // sci_fi
+console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine")) // comedy
+console.log(findKeyByValue(bestTVShowsByGenre, "Yo Mama")) // undefined
