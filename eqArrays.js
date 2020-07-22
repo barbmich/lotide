f15bf7527ff2cf35a0ca757e92afa54545eefb10
Michele Barbiero <babishhh@gmail.com>
2020-07-22 22:11:21 +0000

    commit eqObjects.js and refactored eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -18,8 +18,8 @@ const eqArrays = (array1, array2) => {
   if (array1.length !== array2.length) {
     return false;
   } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
+    for (const index in array1) {
+      if (array1[index] !== array2[index]) {
         return false;
       }
     }
--- eqObjects.js
+++ eqObjects.js
@@ -23,47 +23,33 @@ const eqObjects = function(object1, object2) {
   else {
     for (let key in object1) {
       if (object1[key] != object2[key]) {
-        return false
+        return false;
       }
     }
-    // console.log(`keys of object1: ${Object.keys(object1)}`);
-    // console.log(`keys of object2: ${Object.keys(object2)}`);
-    // if (eqArrays(Object.keys(object1), Object.keys(object2))) {
-    //   console.log(`values of object1: ${Object.values(object1)}`);
-    //   console.log(`values of object2: ${Object.values(object2)}`);
-    //   if (eqArrays(object.values(object1), Object.values(object2))) {
-    //   return true;
-    //   }
-    // };
   }
   return true;
 };
 
-const extractKeys = object => {
-  return Object.keys(object);
-}
+// const eqArrays = (array1, array2) => {
+//   if (array1.length !== array2.length) {
+//     return false;
+//   } else {
+//     for (const index in array1) {
+//       console.log(index)
+//       if (array1[index] !== array2[index]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// };
 
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// eqObjects(ab, ba); // => true
 
+// const abc = { a: "1", b: "2", c: "3"};
+// eqObjects(ab, abc); // => false
 
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // => false
-
-assertEqual(eqObjects(ab, ba), true)
-assertEqual(eqObjects(ab, abc), false)
+// assertEqual(eqObjects(ab, ba), true)
+// assertEqual(eqObjects(ab, abc), false)
