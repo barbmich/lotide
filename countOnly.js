5181839a135c57ffde67515747a50ba64f0d497e
Michele Barbiero <babishhh@gmail.com>
2020-07-22 18:36:08 +0000

    commit countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,50 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    if (typeof actual === 'string') {
+      return console.log(`✅ Assertion Passed! "${actual}" === "${expected}"`);
+    } else {
+      return console.log(`✅ Assertion Passed! ${actual} === ${expected}`);
+    }
+  } else {
+    if (typeof actual === 'string') {
+      return console.log(`🛑 Assertion Failed! "${actual}" !== "${expected}"`);
+    } else {
+      return console.log(`🛑 Assertion Failed! ${actual} !== ${expected}`);
+    }
+  }
+};
+
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
