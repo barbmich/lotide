90acbb14f7517972e9871219fea725963118808f
Michele Barbiero <babishhh@gmail.com>
2020-07-23 23:53:46 +0000

    commit findKey.js, updated assertEqual.js

--- findKey.js
+++ findKey.js
@@ -14,6 +14,33 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// const starSystems = {
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 },
+// }
+
+const findKey = (object, callback) => {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+// const result = findKey(starSystems, x => x.stars === 99) // => "noma"
+
+// const findKeyByValue = (object, value) => {
+//   for (key in object) {
+//     if (object[key] === value) {
+//       return key;
+//     }
+//   }
+// }
+
 // TEST CODE
 // assertEqual("Light", "Light");
 // assertEqual("Lighthouse Labs", "Bootcamp");
