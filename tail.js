686b3b719ab8face378e5c728e20c2bdc4d014ee
Michele Barbiero <babishhh@gmail.com>
2020-07-27 18:49:43 +0000

    moduled tail

--- tail.js
+++ tail.js
@@ -1,23 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    if (typeof actual === 'string') {
-      return console.log(`✅ Assertion Passed! "${actual}" === "${expected}"`);
-    } else {
-      return console.log(`✅ Assertion Passed! ${actual} === ${expected}`);
-    }
-  } else {
-    if (typeof actual === 'string') {
-      return console.log(`🛑 Assertion Failed! "${actual}" !== "${expected}"`);
-    } else {
-      return console.log(`🛑 Assertion Failed! ${actual} !== ${expected}`);
-    }
-  }
-};
-
 const tail = function(array) {
   let newArray = array.slice(0);
   return newArray.slice(1);
 };
+
+module.exports = tail;
 
 // const words = ["Yo Yo", "Lighthouse", "Labs"];
 // tail(words); // no need to capture the return value since we are not checking it
