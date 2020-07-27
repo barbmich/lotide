71df053ebb4893327845b4cf1619c0e62ae9839a
Michele Barbiero <babishhh@gmail.com>
2020-07-27 21:20:25 +0000

    updated index.js to use object property value shorthands

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters = require('./countLetters');
-const eqArrays = require('./eqArrays');
-const countOnly = require('./countOnly');
-const eqObjects = require('./eqObjects');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
-const head = require('./head');
-const letterPositions = require('./letterPositions');
-const map = require('./map');
-const middle = require('./middle');
-const tail   = require('./tail');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
+const assertArraysEqual   = require('./assertArraysEqual');
+const assertEqual         = require('./assertEqual');
+const assertObjectsEqual  = require('./assertObjectsEqual');
+const countLetters        = require('./countLetters');
+const eqArrays            = require('./eqArrays');
+const countOnly           = require('./countOnly');
+const eqObjects           = require('./eqObjects');
+const findKey             = require('./findKey');
+const findKeyByValue      = require('./findKeyByValue');
+const flatten             = require('./flatten');
+const head                = require('./head');
+const letterPositions     = require('./letterPositions');
+const map                 = require('./map');
+const middle              = require('./middle');
+const tail                = require('./tail');
+const takeUntil           = require('./takeUntil');
+const without             = require('./without');
 
 module.exports = {
 
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  eqArrays: eqArrays,
-  countOnly: countOnly,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  head:   head,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  tail:   tail,
-  takeUntil: takeUntil,
-  without: without,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  eqArrays,
+  countOnly,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without,
 
 };
